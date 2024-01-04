0a0e0f8a35e4fad4c952a347eb76da48b125c56e
hsibanda <henrysibanda@hotmail.com.au>
2024-01-04 15:50:29 +0800

    Add pip installation

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ sudo cp dash.conf /etc/nginx/sites-available
 #create symbolic link to enable the config
 sudo ln -s /etc/nginx/sites-available/dash.conf /etc/nginx/sites-enabled/
 
+sudo apt install python3-pip
 pip3 install -r requirements.txt --no-cache-dir
 
 sudo systemctl restart nginx
